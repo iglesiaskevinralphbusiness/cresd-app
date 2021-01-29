5368c1c16ec6b33857896b5e06c3df45c560c1cb
kevinralph <kevin.iglesias@r4pid.com>
2021-01-29 15:52:49 +0800

    Added recipe card component

---
+++ src/components/recipe-cards.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useHistory } from 'react-router-dom';
+import { PAGE_URL } from '../utils/constant/index';
+
+const RecipeCards = ({ recipe }) => {
+    return (
+        <div className="col mb-4">
+            <div className="card h-100">
+                <img src={recipe.images?.full} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{ recipe.title }</h5>
+                    <p className="card-text">{ recipe.description }</p>
+                    <a className="btn btn-primary mr-2" href={`${PAGE_URL.updateRecipe}/${recipe.uuid}`} >Edit</a>
+                    <a className="btn btn-danger">Delete</a>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted">{ recipe.editDate }</small>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RecipeCards;
--- src/views/home.js
+++ src/views/home.js
@@ -2,12 +2,17 @@ import React from "react";
 import FullLayout from '../layout/full-layout';
 import { PAGE_URL } from '../utils/constant/index';
 import { getRecipes } from '../services/services';
+import RecipeCards from '../components/recipe-cards';
 
 class Home extends React.Component {
 
+    state = {
+        recipes: [],
+    }
+
     componentDidMount(){
         getRecipes().then((res) => {
-            console.log(res);
+            this.setState({ recipes: res.data });
         }).catch((error) => {
             console.log(error);
         });
@@ -16,12 +21,24 @@ class Home extends React.Component {
     render() {
         return (
             <FullLayout>
-                <a 
-                    className="btn btn-primary" 
-                    href={PAGE_URL.addRecipe}
-                >
-                    Add Recipe
-                </a>
+                <div>
+                    <a 
+                        className="btn btn-primary mb-5" 
+                        href={PAGE_URL.addRecipe}
+                    >
+                        Add Recipe
+                    </a>
+                </div>
+                <div>
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                        { 
+                            this.state.recipes.map((recipe, index) => {
+                                return <RecipeCards recipe={recipe} key={`recipe_${index}`} />
+                            })
+                        }
+                    </div>
+                </div>
+
             </FullLayout>
         );
     }
