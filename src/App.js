955369fc864ec547d3fbaf0ba8a5ba6526f84a1a
kevinralph <kevin.iglesias@r4pid.com>
2021-01-30 17:04:17 +0800

    clear router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Router from './router/router';
+import './App.css';
+
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './views/recipe';
+import UpdateRecipe from './views/recipe-update';
+import DetailsRecipe from './views/recipe-details';
+import NotFound from './views/404';
 
 function App() {
-  return <Router />;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={Home} exact />
+				<Route path="/add" component={UpdateRecipe}  />
+				<Route path="/update/:id" component={UpdateRecipe}  />
+				<Route path="/view/:id" component={DetailsRecipe}  />
+				<Route path="/not-found" component={NotFound}  />
+				<Redirect to="/not-found" />
+			</Switch>
+		</BrowserRouter>
+	)
 }
 
 export default App;
